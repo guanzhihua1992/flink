943b9bfa8f341e5cd1ab0e610711ec14198cb892
guanzhihua1992 <18300004793@163.com>
2023-07-04 19:59:38 +0800

    add remarks for SessionRestEndpointFactory.java and ClusterEntrypoint.uml

--- flink-runtime/src/main/java/org/apache/flink/runtime/rest/SessionRestEndpointFactory.java
+++ flink-runtime/src/main/java/org/apache/flink/runtime/rest/SessionRestEndpointFactory.java
@@ -39,17 +39,33 @@ public enum SessionRestEndpointFactory implements RestEndpointFactory<Dispatcher
     @Override
     public WebMonitorEndpoint<DispatcherGateway> createRestEndpoint(
             Configuration configuration,
+            //DispatcherGateway服务地址获取器， 用于获取当前活跃的dispatcherGateway地址。
+            // 基于dispatcherGateway可以 实现与Dispatcher的RPC通信，
+            // 最终提交的JobGraph通过 dispatcherGateway发送给Dispatcher组件。
             LeaderGatewayRetriever<DispatcherGateway> dispatcherGatewayRetriever,
+            //ResourceManagerGateway服务地 址获取器，
+            // 用于获取当前活跃的ResourceManagerGateway地址，
+            // 通过 ResourceManagerGateway实现ResourceManager组件之间的RPC通信，
+            // 例如在TaskManagersHandler中通过调用ResourceManagerGateway获取集群中的TaskManagers监控信息。
             LeaderGatewayRetriever<ResourceManagerGateway> resourceManagerGatewayRetriever,
+            //临时二进制对象数据存储服务，
+            // BlobServer 接收数据后，会及时清理Cache中的对象数据。
             TransientBlobService transientBlobService,
+            //用于处理WebMonitorEndpoint请求的线程池服务。
             ScheduledExecutorService executor,
+            //用于拉取JobManager和TaskManager上的Metric监控指标。
             MetricFetcher metricFetcher,
+            //用于在高可用集群中启动和选择服务的 Leader节点，
+            // 如通过leaderElectionService启动WebMonitorEndpoint RPC服 务，
+            // 然后将Leader节点注册至ZooKeeper，以此实现 WebMonitorEndpoint服务的高可用。
             LeaderElectionService leaderElectionService,
+            //异常处理器，当WebMonitorEndpoint出现异常 时调用fatalError-Handler的中处理接口。
             FatalErrorHandler fatalErrorHandler)
             throws Exception {
+        // 通过Configuration获取RestHandlerConfiguration
         final RestHandlerConfiguration restHandlerConfiguration =
                 RestHandlerConfiguration.fromConfiguration(configuration);
-
+        //创建DispatcherRestEndpoint
         return new DispatcherRestEndpoint(
                 dispatcherGatewayRetriever,
                 configuration,
